98b2ba5a73ef7ce05e030bd9d8b91abf387d5d00
andre the <andre.the12@gmail.com>
2017-02-23 10:13:45 +0800

    made a seperate model for customers

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -92,4 +92,41 @@ module.exports = function (passport) {
       }
     })
   }))
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, next) {
+    console.log('test')
+    // Find user with email as given from the callback
+    Customer.findOne({ 'local.email': email }, function (err, foundCustomer) {
+      // if there's a user with the email
+      // call next() middleware with no error arguments + update the flash data
+      if (foundCustomer) {
+        console.log('the same user with same email found')
+        return next(null, false, req.flash('flash', {
+          type: 'warning',
+          message: 'This email is already used'
+        }))
+      } else {
+        // if not found = new user
+        // save user to the db, password is hash
+        // call next() middleware without error argumants
+        let newCustomer = new Customer({
+          local: {
+            email: email,
+            password: User.encrypt(password), // hased(password)
+          }
+        })
+        newCustomer.save(function (err, output) {
+          // function(err, theNewUser, flashData)
+          return next(null, output, req.flash('flash', {
+            type: 'success',
+            message: 'Hello new Guest ' + output.local.email
+          }))
+        })
+      }
+    })
+  }))
 }
