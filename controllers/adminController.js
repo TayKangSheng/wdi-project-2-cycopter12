3d57de536bb6eeff34bcec8c034068de3ee65770
andre the <andre.the12@gmail.com>
2017-02-25 11:02:17 +0800

    refactor admincontroller and adminroutes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,47 +1,40 @@
-// const Admin = require('../models/user')
-// var passport = require('passport')
-//
-// let adminController = {
-//
-//   signup: function (req, res) {
-//     res.render('auth/signup', {
-//       flash: req.flash('flash')[0]
-//     })
-//   },
-//   authCreate: function (req, res) {
-//     var signupStrategy = passport.authenticate('local-signup', {
-//       successRedirect: '/snacks',
-//       failureRedirect: '/signup',
-//       failureFlash: false
-//     })
-//     return signupStrategy(req, res)
-//   },
-//   login: function (req, res) {
-//     res.render('auth/login', {
-//       flash: req.flash('flash')[0]
-//     })
-//   },
-//
-//   authLogin: function (req, res) {
-//     var loginStrategy = passport.authenticate('local-login', {
-//       successRedirect: '/snacks',
-//       failureRedirect: '/login',
-//       failureFlash: false
-//     })
-//     return loginStrategy(req, res)
-//   },
-//
-//   show: function (req, res) {
-//     User.findById(req.params.id, function (err, output) {
-//       if (err) return next(err)
-//       res.render('users/profile', {
-//         userProfile: output
-//       })
-//     })
-//   },
-//   logout: function (req, res) {
-//     req.logout() // remove the session => req.user = undefined, req.isAuthenticated() = false
-//     res.redirect('/')
-//   }
-// }
-// module.exports = adminController
+const Admin = require('../models/user')
+var passport = require('passport')
+
+let adminController = {
+  signup: function (req, res) {
+    res.render('auth/signup', {
+      flash: req.flash('flash')[0]
+    }) // render the form
+  },
+
+  authCreate: function (req, res) {
+    var signupStrategy = passport.authenticate('local-signup', {
+      successRedirect: '/snacks',
+      failureRedirect: '/signup',
+      failureFlash: false
+    })
+    return signupStrategy(req, res)
+  },
+
+  login:function (req, res) {
+      res.render('auth/login',{
+        flash: req.flash('flash')[0]
+      })
+    },
+
+  authLogin: function (req, res, next) {
+    var loginStrategy = passport.authenticate('local-login', {
+      successRedirect: '/snacks',
+      failureRedirect: '/login',
+      failureFlash: false
+    })
+    return loginStrategy(req, res, next)
+  },
+
+  logout: function (req,res){
+    req.logout()//remove the session => req.user = undefined, req.isAuthenticated() = false
+    res.redirect('/')
+  }
+}
+module.exports = adminController
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,56 +2,54 @@ var express = require('express')
 var router = express.Router()
 const isLoggedIn = require('../security/isLoggedIn')
 const isNotLoggedIn = require('../security/isNotLoggedIn')
-// const adminController = require('../controllers/adminController')
-var passport = require('passport')
+const adminController = require('../controllers/adminController')
+// var passport = require('passport')
 
-router.get('/signup', isLoggedIn,function (req, res) {
-  res.render('auth/signup', {
-    flash: req.flash('flash')[0]
-  }) // render the form
-})
-
-router.post('/signup', function (req, res) {
-  console.log('heeelelele');
-  var signupStrategy = passport.authenticate('local-signup', {
-    successRedirect: '/snacks',
-    failureRedirect: '/signup',
-    failureFlash: false
-  })
-  return signupStrategy(req, res)
-})
-
-router.get('/login', isLoggedIn,function (req, res) {
-    res.render('auth/login',{
-      flash: req.flash('flash')[0]
-    })
-  })
-
-router.post('/login', function (req, res, next) {
-  var loginStrategy = passport.authenticate('local-login', {
-    successRedirect: '/snacks',
-    failureRedirect: '/login',
-    failureFlash: false
-  })
-  return loginStrategy(req, res, next)
-})
-
-router.get('/logout', function (req,res){
-  req.logout()//remove the session => req.user = undefined, req.isAuthenticated() = false
-  res.redirect('/')
-})
-
-// router.get('/signup', adminController.signup)
+// router.get('/signup', isLoggedIn,function (req, res) {
+//   res.render('auth/signup', {
+//     flash: req.flash('flash')[0]
+//   }) // render the form
+// })
 //
-// router.post('/signup', adminController.authCreate)
+// router.post('/signup', function (req, res) {
+//   console.log('heeelelele');
+//   var signupStrategy = passport.authenticate('local-signup', {
+//     successRedirect: '/snacks',
+//     failureRedirect: '/signup',
+//     failureFlash: false
+//   })
+//   return signupStrategy(req, res)
+// })
 //
-// router.get('/login', adminController.login)
+// router.get('/login', isLoggedIn,function (req, res) {
+//     res.render('auth/login',{
+//       flash: req.flash('flash')[0]
+//     })
+//   })
 //
-// router.post('/login', adminController.authLogin)
+// router.post('/login', function (req, res, next) {
+//   var loginStrategy = passport.authenticate('local-login', {
+//     successRedirect: '/snacks',
+//     failureRedirect: '/login',
+//     failureFlash: false
+//   })
+//   return loginStrategy(req, res, next)
+// })
 //
-// router.get('/users/:id', adminController.show)
-//
-// router.get('/logout', adminController.logout)
+// router.get('/logout', function (req,res){
+//   req.logout()//remove the session => req.user = undefined, req.isAuthenticated() = false
+//   res.redirect('/')
+// })
+
+router.get('/signup',isLoggedIn, adminController.signup)
+
+router.post('/signup', adminController.authCreate)
+
+router.get('/login', isLoggedIn,adminController.login)
+
+router.post('/login', adminController.authLogin)
+
+router.get('/logout', adminController.logout)
 
 
 module.exports = router
